93b84d73029ae9b2e3a258a02416f27b34be5e2a
SvetlanaBuuvol <divinesvetok@gmail.com>
2023-08-10 02:24:55 +0300

    +

--- src/redax/store.js
+++ src/redax/store.js
@@ -1,5 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
-import { persistStore } from 'redux-persist';
+import {
+  persistStore,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist'
 import { contactsReducer } from './contactsSlice';
 import { filterSlice } from './filterSlice';
 
@@ -8,6 +16,12 @@ const store = configureStore({
     contacts: contactsReducer,
     filter: filterSlice.reducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
 
 export const persistor = persistStore(store);
